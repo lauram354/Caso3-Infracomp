97c1f7f8f060e436cdca71ddba62ebf5e0241430
lauram354 <l.murciac@uniandes.edu.co>
2024-11-03 01:20:50 -0500

    extrasss

--- ProtocoloCliente.java
+++ ProtocoloCliente.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.security.SecureRandom;
 
 public class ProtocoloCliente {
     public static void ejecucion(BufferedReader entrada, BufferedReader servIn, PrintWriter salida) throws IOException{
@@ -10,6 +12,20 @@ public class ProtocoloCliente {
         user = "Hola :)";
         System.out.println("Mensaje enviado " + user);     
         salida.println(user);
+
+        salida.println("SECINIT");
+        BigInteger reto = generarReto(1024); //TODO: revisar longitud del reto 
+        //TODO: Revisar como obtener la llave
+        //Key llave = 
+        //byte[] R = Asimetrico.cifrar(null, "RSA", reto);
+
             
     }
+
+    public static BigInteger generarReto(int longitud){
+        SecureRandom aleatorio = new SecureRandom();
+        BigInteger reto = new BigInteger(longitud, aleatorio);
+        return reto;
+    }
 }
+
---
+++ Simetrico.java
@@ -0,0 +1,56 @@
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+public class Simetrico {
+    private final static String PADDING = "AES/ECB/PKCS5Padding";
+
+    public static byte[] cifrar(SecretKey llave, String mensaje){
+        byte[] cifrado;
+
+        try {
+            Cipher cifrador = Cipher.getInstance(PADDING);
+            byte[] mensajeBytes = mensaje.getBytes();
+            cifrador.init(Cipher.ENCRYPT_MODE, llave);
+            cifrado = cifrador.doFinal(mensajeBytes);
+            return cifrado;
+        } catch (Exception e) {
+            // TODO: handle exception
+            return null;
+        }
+    }
+    
+    public static byte[] descifrar(SecretKey llave, byte[] mensaje){
+        byte[] descifrado;
+
+        try {
+            Cipher cifrador = Cipher.getInstance(PADDING);
+            cifrador.init(Cipher.DECRYPT_MODE, llave);
+            descifrado = cifrador.doFinal(mensaje);
+        } catch (Exception e) {
+            // TODO: handle exception
+            return null;
+        }
+        return descifrado;
+    }
+
+    public static void generarLlave() throws NoSuchAlgorithmException{
+        KeyGenerator keygen = KeyGenerator.getInstance(PADDING);
+        SecretKey secretKey = keygen.generateKey();
+        
+    }
+
+    public static void imprimir (byte[] contenido) throws UnsupportedEncodingException{
+        int i = 0;
+        for (; i < contenido.length -1; i++){
+            System.out.print(contenido[i] + " ");
+        }
+        System.out.print(contenido[i] + " ");
+    }
+}
